e907c25f675e8fc95ad47aabcda499b1cd373bc0
dngeonMasteer <kaushikborah4080@gmail.com>
2025-01-20 01:34:13 +0530

    Frontend Done

--- cient/src/assets/assets.js
+++ cient/src/assets/assets.js
@@ -58,19 +58,19 @@ export const testimonialsData = [
 export const plans = [
     {
       id: 'Basic',
-      price: 10,
+      price: 1,
       credits: 100,
       desc: 'Best for personal use.'
     },
     {
       id: 'Advanced',
-      price: 50,
+      price: 5,
       credits: 500,
       desc: 'Best for business use.'
     },
     {
       id: 'Business',
-      price: 250,
+      price: 25,
       credits: 5000,
       desc: 'Best for enterprise use.'
     },
